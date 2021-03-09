e25f9c2e60021b9985adaff1b9c9017f1fc11ce5
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2021-03-09 21:46:17 +0100

    Update pin.py

--- pin.py
+++ pin.py
@@ -1,9 +1,10 @@
 a = int(input())
-x = list(map(int, input().split()))
+x = input()
+x = x.replace(' ', '')
 c = 0
 maxi = 0
-for tmp in x: 
-    if tmp == 1: c = c+1
+for i in x:
+    if i == '1': c = c+1
     else:
         if(maxi < c): maxi = c
         c = 0
